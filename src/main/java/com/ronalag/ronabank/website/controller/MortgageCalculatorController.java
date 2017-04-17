906827a9c6b8890c073c09410a5da29d314feedb
Henry Aghaulor <henry.aghaulor@outlook.com>
2017-04-17 19:11:13 -0400

    Added error validation for mortgage input

--- src/main/java/com/ronalag/ronabank/website/controller/MortgageCalculatorController.java
+++ src/main/java/com/ronalag/ronabank/website/controller/MortgageCalculatorController.java
@@ -1,6 +1,7 @@
 package com.ronalag.ronabank.website.controller;
 
 import java.util.Map;
+import java.util.Vector;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -22,6 +23,8 @@ public class MortgageCalculatorController {
 
 	private static final String AMORTIZATION_ATTRIBUTE = "amortization";
 	
+	private static final String DECIMAL_ERROR_PART = " must be a decimal value.";
+	
 	private static final int DEFAULT_AMORTIZATION = 25;
 	
 	private static final float DEFAULT_DOWN_PAYMENT = (float) 40000.0;
@@ -31,6 +34,8 @@ public class MortgageCalculatorController {
 	private static final float DEFAULT_PURCHASE_PRICE = (float) 200000.0;
 	
 	private static final String DOWN_PAYMENT_ATTRIBUTE = "downPayment";
+	
+	private static final String ERROR_ATTRIBUTE = "errors";
 	
 	private static final String INTEREST_RATE_ATTRIBUTE = "interestRate";
 	
@@ -65,13 +70,15 @@ public class MortgageCalculatorController {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public String post(@RequestParam Map<String, String> params, Model model) {
-		MortgageCalculatorInputBean input = getMortgageCalculatorInput(params);
-		
-		if (params == null || !params.containsKey(SUBMIT_PARAM) || params.get(SUBMIT_PARAM) == null) {
-			model.addAttribute(MORTGAGE_INPUT_ATTRIBUTE, input);
+		boolean isValid = validateInput(params, model);
+						
+		if (!isValid || !params.containsKey(SUBMIT_PARAM) || params.get(SUBMIT_PARAM) == null) {
+			model.addAttribute(MORTGAGE_INPUT_ATTRIBUTE, getDefaultMortgageCalculatorInput());
 			return MORTGAGE_VIEW;
-		}
-		
+		}		
+
+		MortgageCalculatorInputBean input = getMortgageCalculatorInput(params);
+		model.addAttribute(MORTGAGE_INPUT_ATTRIBUTE, input);
 		String submit = params.get(SUBMIT_PARAM);
 		
 		if (submit.equals(SUBMIT_VALUE)) {
@@ -100,6 +107,30 @@ public class MortgageCalculatorController {
 		return input;
 	}
 	
+	private Float getFloatValue(String stringValue) {
+		Float value;
+		
+		try {
+			value = Float.parseFloat(stringValue);
+		} catch (NumberFormatException nfe) {
+			value = null;
+		}
+		
+		return value;
+	}
+	
+	private Integer getIntegerValue(String stringValue) {
+		Integer value;
+		
+		try {
+			value = Integer.parseInt(stringValue);
+		} catch (NumberFormatException nfe) {
+			value = null;
+		}
+		
+		return value;
+	}
+	
 	private MortgageCalculatorInputBean getMortgageCalculatorInput(Map<String, String> params) {		
 		MortgageCalculatorInputBean input = getDefaultMortgageCalculatorInput();
 		
@@ -124,5 +155,73 @@ public class MortgageCalculatorController {
 		}
 		
 		return input;
+	}
+	
+	private boolean validateInput(Map<String, String> params, Model model) {
+		if (params == null || model == null) {
+			return false;
+		}
+		
+		Vector<String> errors = new Vector<String>();
+		Integer amortization = null;
+		if (params.containsKey(AMORTIZATION_ATTRIBUTE)) {
+			amortization = getIntegerValue(params.get(AMORTIZATION_ATTRIBUTE));
+		}
+				
+		if (amortization == null) {
+			errors.add("Amortization must be an integer");
+		} else if (amortization <= 0 || amortization > 40) {
+			errors.add("Amortization must be greater than zero and no greater than forty.");
+		}
+		
+		Float purchasePrice = null;
+		if (params.containsKey(PURCHASE_PRICE_ATTRIBUTE)) {
+			purchasePrice = getFloatValue(params.get(PURCHASE_PRICE_ATTRIBUTE));
+		}
+		
+		if (purchasePrice == null) {
+			errors.addElement("Purchase price " + DECIMAL_ERROR_PART);
+		} else if (purchasePrice <= 0){
+			errors.add("Purchase price must be greater than zero.");
+		}
+		
+		Float downPayment = null;
+		if (params.containsKey(DOWN_PAYMENT_ATTRIBUTE)) {
+			downPayment = getFloatValue(params.get(DOWN_PAYMENT_ATTRIBUTE));
+		}
+		
+		if (downPayment == null) {
+			errors.add("Down Payment " + DECIMAL_ERROR_PART);
+		} else {
+			if (downPayment <= 0) {
+				errors.add("Down payment must be greater than zero");
+			}
+			
+			if (purchasePrice != null && downPayment >= purchasePrice) {
+				errors.add("Down payment should be greater than the purchase price.");
+			} 
+		}
+				
+		Float interestRate = null;
+		if (params.containsKey(INTEREST_RATE_ATTRIBUTE)) {
+			interestRate = getFloatValue(params.get(INTEREST_RATE_ATTRIBUTE));
+		}
+		
+		if (interestRate == null) {
+			errors.add("Interest rate must be greater than zero");
+		} else {
+			if (interestRate <= 0 || interestRate > 100) {
+				errors.add("Interest rate should be greater than zero and less than one hundred");
+			}			
+		}
+		
+		int length = errors.size();
+		
+		if (length > 0) {
+			model.addAttribute("errors", errors);
+		}
+		
+		return length == 0;
+		
 	}
 }
